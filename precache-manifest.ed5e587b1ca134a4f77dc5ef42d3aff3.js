d1fbbf34d9cef87bb3eed2cfd88730e038598a87
Yiming Wang <zdwym1997@gmail.com>
2020-05-25 02:29:31 -0700

    Updates

---
+++ precache-manifest.ed5e587b1ca134a4f77dc5ef42d3aff3.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3be9fa9aae2c9bef739309975141ce",
+    "url": "/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "54cb4d56e04c16c3119d",
+    "url": "/PersonalWebsite/static/css/main.aa295ac4.chunk.css"
+  },
+  {
+    "revision": "a6bbb99a03b94f96ea1d",
+    "url": "/PersonalWebsite/static/js/2.4dffb3a7.chunk.js"
+  },
+  {
+    "revision": "4ffd636a8da1eb6549dae7128f8e9692",
+    "url": "/PersonalWebsite/static/js/2.4dffb3a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cb4d56e04c16c3119d",
+    "url": "/PersonalWebsite/static/js/main.cdcbe68e.chunk.js"
+  },
+  {
+    "revision": "8ca75ff444eb7c4c59f5",
+    "url": "/PersonalWebsite/static/js/runtime-main.159c0f3d.js"
+  },
+  {
+    "revision": "40b85bd3e24d11e9c221a6e83ad5dc8f",
+    "url": "/PersonalWebsite/static/media/Me.40b85bd3.png"
+  },
+  {
+    "revision": "a8a01a9920435a43f5fa501037d91ff2",
+    "url": "/PersonalWebsite/static/media/bgi.a8a01a99.jpg"
+  },
+  {
+    "revision": "5d1f2b8a821e17e2b36723bfc3b37299",
+    "url": "/PersonalWebsite/static/media/capture1.5d1f2b8a.png"
+  },
+  {
+    "revision": "476006154c35fa128d1fba5e32ec1a7b",
+    "url": "/PersonalWebsite/static/media/capture2.47600615.png"
+  },
+  {
+    "revision": "5c375df7d7a0f934f5be73317b87fdd2",
+    "url": "/PersonalWebsite/static/media/capture3.5c375df7.png"
+  },
+  {
+    "revision": "5c94fb00fe650fb3c9a1820c55a1458a",
+    "url": "/PersonalWebsite/static/media/capture4.5c94fb00.png"
+  },
+  {
+    "revision": "54f938677263bf4a5b8de31379d2f0c8",
+    "url": "/PersonalWebsite/static/media/csharp.54f93867.png"
+  },
+  {
+    "revision": "9ad79828751ede5dbe9f92f97237bdf2",
+    "url": "/PersonalWebsite/static/media/firebase.9ad79828.png"
+  },
+  {
+    "revision": "ecc4cf252a967f043b7cc229560486ac",
+    "url": "/PersonalWebsite/static/media/github.ecc4cf25.png"
+  },
+  {
+    "revision": "e9b6b11823b3aac1438beca790c8f26a",
+    "url": "/PersonalWebsite/static/media/googlecloud.e9b6b118.png"
+  },
+  {
+    "revision": "4f155afb4ac98aba4db885e1a2b7af6a",
+    "url": "/PersonalWebsite/static/media/java.4f155afb.png"
+  },
+  {
+    "revision": "d5e08cd1bc2797eb9f6bcdf90dea6806",
+    "url": "/PersonalWebsite/static/media/react.d5e08cd1.png"
+  },
+  {
+    "revision": "3c5bbca93af01fc4165b102e42ee022e",
+    "url": "/PersonalWebsite/static/media/unity.3c5bbca9.png"
+  }
+]);
